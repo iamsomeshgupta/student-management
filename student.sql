1a3c3324cf6b8416aea1eeb7bd85cacac3463200
iamsomeshgupta <somesh1392005@gamil.com>
2026-01-19 00:56:37 +0530

    project started

---
+++ student.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE student_system;
+\c student_system
+CREATE TABLE students(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email varchar(150) unique not null,
+    age int CHECK(age>=16),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+create table courses(
+    id serial 
+)
